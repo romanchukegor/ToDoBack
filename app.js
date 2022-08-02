e4c1acf4525c7e967beff7760a13304ed44ce9b6
romanchukegor <romanchuk.fortech@gmail.com>
2022-08-02 17:12:17 +0300

    commit

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-const apiRoutes = require('./src/modules/routes/routes')
+const apiRoutes = require("./src/modules/routes/routes");
 
-app.use(cors())
+app.use(cors());
 
-const uri = 'mongodb+srv://armavir:armavir@cluster0.1bdo7.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true })
+const uri =
+  "mongodb+srv://armavir:armavir@cluster0.1bdo7.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use('/', apiRoutes)
-
+app.use("/", apiRoutes);
 
 app.listen(8000, () => {
   console.log(`Server running at port 8000`);
 });
-
